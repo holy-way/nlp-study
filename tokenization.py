abac10ecfe22b023c617a46906fe7dbc5edda04c
TH <rundun159@naver.com>
2025-03-05 02:32:58 +0900

    Update tokenization.py

--- tokenization.py
+++ tokenization.py
@@ -5,7 +5,16 @@ from nltk.tokenize import word_tokenize
 from nltk.tokenize import WordPunctTokenizer
 from tensorflow.keras.preprocessing.text import text_to_word_sequence
 
+ex_str = "Don't be fooled by the dark sounding name, Mr. Jone's Orphanage is as cheery as cheery goes for a pastry shop."
 
-print('단어 토큰화1 :',
-	word_tokenize("Don't be fooled by the dark sounding name, Mr. Jone's Orphanage is as cheery as cheery goes for a pastry shop.")
+print('word_tokenize :',
+	word_tokenize(ex_str)
+)
+
+print('WordPunctTokenizer :',
+	WordPunctTokenizer().tokenize(ex_str)
+)
+
+print('text_to_word_sequence : ',
+	text_to_word_sequence(ex_str)
 )
